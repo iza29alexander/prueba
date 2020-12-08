6f95e3fb9088c8dee933f4b19587e69bc6073e28
Edi <alex-edy175@hotmail.com>
2020-12-07 21:22:14 -0500

    prueba de configuracion

---
+++ src/semana1aiza/Semana1AIza.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana1aiza;
+
+/**
+ *
+ * @author Edi
+ */
+public class Semana1AIza {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmPrincipal v_principal=new FrmPrincipal();
+        v_principal.show(true);
+    }
+    
+}
